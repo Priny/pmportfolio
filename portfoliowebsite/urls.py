4970a62061c2f54420c5463a04bc047fb365170b
Shifa Davis <shifadavis10@gmail.com>
2023-04-05 22:17:49 +0530

    added a /homepage in the urls.py original

--- portfoliowebsite/urls.py
+++ portfoliowebsite/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('portfolio.urls')),
+    path('', include('portfolio.urls')),
+    path('/homepage', include('portfolio.urls')),
 ]
