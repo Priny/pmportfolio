96ea1d92163ce176ae1f0e7157409fe528e6b8c1
Shifa Davis <shifadavis10@gmail.com>
2023-04-05 20:34:20 +0530

    Initial commit

---
+++ portfolio/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from . import views
+
+
+urlpatterns = [
+    path('', views.homepage, name='homepage'),
+    path('workexperience/', views.workexperience, name='workexperience'),
+    path('project/', views.project, name='project'),
+    path('education/', views.education, name='education'),
+    path('aboutme/', views.aboutme, name='aboutme'),
+    path('contact/', views.contact, name='contact'),
+    path('reviews/', views.reviews, name='reviews'),
+
+]
