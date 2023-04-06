510f23dbd212bf77c01c5e95eace47db682ed459
Shifa Davis <shifadavis10@gmail.com>
2023-04-06 23:40:11 +0530

    DIRS

--- portfoliowebsite/settings.py
+++ portfoliowebsite/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'portfoliowebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, 'templates'],
+        'DIRS': ['/home/SDavis/pmportfolio/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
