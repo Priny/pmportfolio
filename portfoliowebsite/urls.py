16fa4e203770b7113e3d987a6f75a39daca3c5c1
Shifa Davis <shifadavis10@gmail.com>
2023-04-06 10:14:00 +0530

    removed a /homepage in the urls.py original

--- portfoliowebsite/urls.py
+++ portfoliowebsite/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),
-    path('/homepage', include('portfolio.urls')),
+
+
 ]
